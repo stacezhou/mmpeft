d783cf77fc64c09f262cd3a6dfa963aa9b791be4
stacezhou <stacezhou@gmail.com>
2023-04-14 13:15:50 +0000

    add myconfigs

--- myconfigs/clip_vit_b32.py
+++ myconfigs/clip_vit_b32.py
@@ -1,8 +1,8 @@
 _base_ = [
-    'dtd_dataset.py',
+    'datasets/dtd_dataset.py',
     'CLIP/clip-vit-base-p32.py',
-    'debug_schedule.py',
-    '_base_/default_runtime.py'
+    'schedules/default_runtime.py'
+    'schedules/default_schedule.py'
 ]
 load_from = 'data/clip-ViT-B-32.pth'
 model = dict(
