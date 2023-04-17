dfdd38d666a21d1fe073b991942191d4c85123d1
stacezhou <stacezhou@gmail.com>
2023-04-17 08:10:02 +0000

    commit

--- myconfigs/clip_vit_b32.py
+++ myconfigs/clip_vit_b32.py
@@ -2,7 +2,6 @@ _base_ = [
     'datasets/imagenet_224.py',
     'CLIP/clip-vit-base-p32.py',
     'schedules/default_runtime.py',
-    'schedules/default_schedule.py'
 ]
 # load_from = 'data/clip-ViT-B-32.pth'
 model = dict(
@@ -14,3 +13,14 @@ model = dict(
     #     pattern = 'visual', # 只微调视觉分支
     # )
 )
+optim_wrapper = dict(
+    optimizer=dict(type='AdamW', lr=1e-5))
+
+# learning policy
+param_scheduler = dict(
+    type='MultiStepLR', by_epoch=True, milestones=[12, 16, 20], gamma=0.1)
+
+# train, val, test setting
+train_cfg = dict(by_epoch=True, max_epochs=20, val_interval=1)
+val_cfg = dict()
+test_cfg = dict()
